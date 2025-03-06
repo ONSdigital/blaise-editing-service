487fe620984e5a05e8009ce5076ffa0d5fc2a958
kristian4res <57638182+kristian4res@users.noreply.github.com>
2025-03-06 10:55:01 +0000

    buid/test: migrate from jest to vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom', 
+        setupFiles: './src/setupTests.ts',
+        include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
         coverage: {
             reporter: ['text', 'json', 'html'],
         },
