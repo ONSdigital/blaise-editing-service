3806fc8ba38d8830abea4960765e8e269a18346a
willst1 <62559442+Willst1@users.noreply.github.com>
2025-03-07 11:29:56 +0000

    config: update package.json scripts and remove proxy configuration from vite.config.mts

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,10 +9,5 @@ export default defineConfig({
         coverage: {
             reporter: ['text', 'json', 'html'],
         },
-        poolOptions: {
-            forks: {
-                minForks: 1,
-            }
-        },
     },
 })
