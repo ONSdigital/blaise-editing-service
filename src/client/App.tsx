7fb0f07713f72bfb6ab885bb31af7c030f75988a
SidraJaved <16195224+SidraJaved@users.noreply.github.com>
2025-04-14 09:44:37 +0100

    wip: font changes & production env warning added to App.tsx
    Refs: BLAIS5-4666

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,14 +3,22 @@ import { ReactElement } from 'react';
 import { Authenticate } from 'blaise-login-react-client';
 import AppRoutes from './Common/components/AppRoutes';
 import LayoutTemplate from './Common/components/LayoutTemplate';
+import { isProduction } from './env';
+import { NotProductionWarning } from 'blaise-design-system-react-components';
 
 function App(): ReactElement {
   return (
-    <Authenticate title="Blaise editing service">
+    <Authenticate title="Blaise Editing Service">
       {(user, loggedIn, logOutFunction) => (
-        <LayoutTemplate showSignOutButton={loggedIn} signOut={() => logOutFunction()}>
-          <AppRoutes user={user} />
-        </LayoutTemplate>
+        <>
+          <a className="ons-skip-link">Skip to content</a>
+          {
+            isProduction(window.location.hostname) ? <></> : <NotProductionWarning />
+          }
+          <LayoutTemplate showSignOutButton={loggedIn} signOut={() => logOutFunction()}>
+            <AppRoutes user={user} />
+          </LayoutTemplate>
+        </>
       )}
     </Authenticate>
 
