3cf1bbef74666b3ffd9282a1c74e73be08466ec7
SidraJaved <16195224+SidraJaved@users.noreply.github.com>
2025-04-15 13:39:23 +0100

    wip: design improvements,
    Refs: BLAIS5-4666

--- src/client/Support/Components/SupportContent.tsx
+++ src/client/Support/Components/SupportContent.tsx
@@ -16,18 +16,16 @@ export default function SupportContent({ questionnaire }: RSupportContentProps):
           className="ons-metadata ons-metadata__list ons-grid ons-grid--gutterless ons-u-cf ons-u-mb-no"
           title="Questionnares"
           data-testid={`${questionnaire.questionnaireName}-Support-Content`}
-          style={{ padding: '0 0 15px 5px' }}
         >
-          <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Field period:</dt>
-          <dd className="ons-description-list__value ons-grid__col ons-col-7@m">{questionnaire.fieldPeriod}</dd>
-          <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Total number of cases:</dt>
-          <dd className="ons-description-list__value ons-grid__col ons-col-7@m">{questionnaire.numberOfCases}</dd>
+          <dt className="ons-description-list__term ons-grid__col ons-col-6@m">Field period</dt>
+          <dd className="ons-description-list__value ons-grid__col ons-col-6@m">{questionnaire.fieldPeriod}</dd>
+          <dt className="ons-description-list__term ons-grid__col ons-col-6@m">Total number of cases</dt>
+          <dd className="ons-description-list__value ons-grid__col ons-col-6@m">{questionnaire.numberOfCases}</dd>
         </dl>
       </ONSPanel>
       <br />
-
       <CaseSearchForm questionnaireName={questionnaire.questionnaireName} userRole={UserRole.Survey_Support} />
 
-    </div>
+    </div >
   );
 }
