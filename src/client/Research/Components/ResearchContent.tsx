aade2c1e5c887a09b94044fc7ebbb626a4cf76df
SidraJaved <16195224+SidraJaved@users.noreply.github.com>
2025-04-17 13:13:24 +0100

    wip: research role updates done, with additional fix for BLAIS5-4754 to handle corner case for reallocation leading to error "something went wrong"
    Refs: BLAIS5-4666

--- src/client/Research/Components/ResearchContent.tsx
+++ src/client/Research/Components/ResearchContent.tsx
@@ -1,5 +1,4 @@
 import { ReactElement } from 'react';
-import { ONSPanel } from 'blaise-design-system-react-components';
 import { QuestionnaireDetails } from '../../../common/interfaces/surveyInterface';
 import CaseSearchForm from '../../Common/components/CaseSearchForm';
 import UserRole from '../../Common/enums/UserTypes';
@@ -11,19 +10,44 @@ interface ResearchContentProps {
 export default function ResearchContent({ questionnaire }: ResearchContentProps): ReactElement {
   return (
     <div className="questionnaire">
-      <ONSPanel status="info">
-        <dl
-          className="ons-metadata ons-metadata__list ons-grid ons-grid--gutterless ons-u-cf ons-u-mb-no"
-          title="Questionnares"
-          data-testid={`${questionnaire.questionnaireName}-Research-Content`}
-          style={{ padding: '0 0 15px 5px' }}
-        >
-          <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Field period:</dt>
-          <dd className="ons-description-list__value ons-grid__col ons-col-7@m">{questionnaire.fieldPeriod}</dd>
-          <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Total number of cases:</dt>
-          <dd className="ons-description-list__value ons-grid__col ons-col-7@m">{questionnaire.numberOfCases}</dd>
-        </dl>
-      </ONSPanel>
+      <div className="ons-summary ons-u-mb-m">
+        <div className="ons-summary__group">
+          <table className="ons-summary__items" data-testid={`${questionnaire.questionnaireName}-Research-Content`}>
+            <thead className="ons-u-vh">
+              <tr>
+                <th>Detail</th>
+                <th>Output</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="ons-summary__row ons-summary__row--has-values">
+                <td className="ons-summary__item-title">
+                  <div className="ons-summary__item--text">
+                    Field Period
+                  </div>
+                </td>
+                <td className="ons-summary__values" colSpan={2}>
+                  {questionnaire.fieldPeriod}
+                </td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr className="ons-summary__row ons-summary__row--has-values">
+                <td className="ons-summary__item-title">
+                  <div className="ons-summary__item--text">
+                    Total number of cases
+                  </div>
+                </td>
+                <td className="ons-summary__values" colSpan={2}>
+                  {questionnaire.numberOfCases}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+      </div>
+
       <br />
 
       <CaseSearchForm questionnaireName={questionnaire.questionnaireName} userRole={UserRole.FRS_Researcher} />
