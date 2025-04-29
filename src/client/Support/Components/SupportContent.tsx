2d470fd2d2c139daf93c02300386c85c59d925d4
SidraJaved <16195224+SidraJaved@users.noreply.github.com>
2025-04-29 09:46:41 +0100

    :/ testing with change in component

--- src/client/Support/Components/SupportContent.tsx
+++ src/client/Support/Components/SupportContent.tsx
@@ -16,7 +16,6 @@ export default function SupportContent({ questionnaire }: RSupportContentProps):
           className="ons-metadata ons-metadata__list ons-grid ons-grid--gutterless ons-u-cf ons-u-mb-no"
           title="Questionnares"
           data-testid={`${questionnaire.questionnaireName}-Support-Content`}
-          style={{ padding: '0 0 15px 5px' }}
         >
           <dt className="ons-description-list__term ons-grid__col ons-col-5@m">Field period:</dt>
           <dd className="ons-description-list__value ons-grid__col ons-col-7@m">{questionnaire.fieldPeriod}</dd>
