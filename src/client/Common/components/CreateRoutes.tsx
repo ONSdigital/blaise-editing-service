e72899637aff68f2872748c2167b5acc6008f54c
kristian4res <57638182+kristian4res@users.noreply.github.com>
2025-05-14 18:04:27 +0100

    feat: add 404 handling for unmatched API endpoints and improve CreateRoutes component with default not found route

--- src/client/Common/components/CreateRoutes.tsx
+++ src/client/Common/components/CreateRoutes.tsx
@@ -1,14 +1,24 @@
 import { ReactNode } from 'react';
-import { Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 /**
  * Only allows navigation to a route if a condition is met.
  * Otherwise, it redirects to a different specified route.
  */
 
+// Simple default 404 component
+function DefaultNotFound(): JSX.Element {
+  return <div>Page not found</div>;
+}
+
 /* eslint-disable react/jsx-no-useless-fragment */
 export default function CreateRoutes({ onConditionThat, children }: CreateRoutesProps): JSX.Element {
-  return onConditionThat ? <Routes>{children}</Routes> : <></>;
+  return onConditionThat ? (
+    <Routes>
+      {children}
+      <Route path="*" element={<DefaultNotFound />} />
+    </Routes>
+  ) : <></>;
 }
 
 export type CreateRoutesProps = {
