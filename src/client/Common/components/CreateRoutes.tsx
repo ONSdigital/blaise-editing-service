b5bc65a68aabde76ecdac3bb89fa3d7855031d8c
kristian4res <57638182+kristian4res@users.noreply.github.com>
2025-05-16 11:08:21 +0100

    revert

--- src/client/Common/components/CreateRoutes.tsx
+++ src/client/Common/components/CreateRoutes.tsx
@@ -1,35 +1,14 @@
 import { ReactNode } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 
 /**
  * Only allows navigation to a route if a condition is met.
  * Otherwise, it redirects to a different specified route.
  */
 
-function DefaultNotFound(): JSX.Element {
-  return (
-    <div className="ons-grid">
-      <div className="ons-grid__col ons-col-8@m">
-        <main id="page-main-content" className="ons-page__main ">
-          <h1>404 Not Found</h1>
-          <p>
-            <a href="/">Back to the homepage</a>
-            .
-          </p>
-        </main>
-      </div>
-    </div>
-  );
-}
-
 /* eslint-disable react/jsx-no-useless-fragment */
 export default function CreateRoutes({ onConditionThat, children }: CreateRoutesProps): JSX.Element {
-  return onConditionThat ? (
-    <Routes>
-      {children}
-      <Route path="*" element={<DefaultNotFound />} />
-    </Routes>
-  ) : <></>;
+  return onConditionThat ? <Routes>{children}</Routes> : <></>;
 }
 
 export type CreateRoutesProps = {
