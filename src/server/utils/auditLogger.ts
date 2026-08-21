6e96a37bb9dce5072dcb62e06eb322214ece5d2c
Richmond Rice <richmond@ricey.org>
2026-08-21 09:12:43 +0100

    sanitise logs

--- src/server/utils/auditLogger.ts
+++ src/server/utils/auditLogger.ts
@@ -1,15 +1,15 @@
 import { type IncomingMessage } from "http";
 
-import type { SanitisedLogString } from "./sanitisation.js";
+import { type SanitisedLogString, sanitiseForLogging } from "./sanitisation.js";
 
 export default class AuditLogger {
   constructor(_projectId: string, _moduleId = process.env.GAE_SERVICE ?? "bes-ui") {}
 
   info(logger: IncomingMessage["log"], message: SanitisedLogString): void {
-    logger.info(message);
+    logger.info(sanitiseForLogging(message));
   }
 
   error(logger: IncomingMessage["log"], message: SanitisedLogString): void {
-    logger.error(message);
+    logger.error(sanitiseForLogging(message));
   }
 }
